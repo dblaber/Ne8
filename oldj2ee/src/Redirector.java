eb83142ae3a968b2f1e95284e7bc5f094c9a8c97
Darren Blaber <dmbtech@gmail.com>
2010-11-16 22:41:24 -0500

    Import old experiment with j2ee.

---
+++ oldj2ee/src/Redirector.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Redirector
+ */
+public class Redirector extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	ServletConfig config;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Redirector() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		this.config = config;
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return config;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getPathInfo().equals("/"))
+		{
+			response.getWriter().write("HomePage");
+		}
+		else
+		{
+			response.getWriter().write("Got: " + request.getPathInfo().substring(1));
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
